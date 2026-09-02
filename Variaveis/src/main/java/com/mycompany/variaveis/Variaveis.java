e3ca0391cc3015c5c91a6915636277453d5ff6d4
gdu9504-hash <gdu9504@gmail.com>
2026-09-02 17:55:37 -0300

    aprendendo sobre variaveis

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author shpctac1000d
+ */
+public class Variaveis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Eu sou o Daniel ");
+    }
+}
